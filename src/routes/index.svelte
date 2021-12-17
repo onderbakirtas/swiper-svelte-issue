6176c2bd48d0509a02ff85efda864abe1e4905a1
Onder Bakirtas <onderbakirtas@gmail.com>
2021-12-17 16:46:23 +0300

    add swiper slides

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { Swiper, SwiperSlide } from "swiper/svelte";
+  import 'swiper/css'
+</script>
+
+<Swiper class="hi" slidesPerView={2} loop={true}>
+  <SwiperSlide>Slide 1</SwiperSlide>
+  <SwiperSlide>Slide 2</SwiperSlide>
+  <SwiperSlide>Slide 3</SwiperSlide>
+  <SwiperSlide>Slide 4</SwiperSlide>
+  <SwiperSlide>Slide 5</SwiperSlide>
+</Swiper>
+
+<style>
+  :global(*) {
+    box-sizing: border-box;
+  }
+  :global(body) {
+    margin: 0;
+  }
+  :global(.hi) {
+    height: 100vh;
+  }
+  :global(.swiper-slide) {
+    border: 3px solid red;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 3rem;
+  }
+</style>
